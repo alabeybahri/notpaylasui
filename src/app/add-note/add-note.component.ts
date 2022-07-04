bcba89c38c9c6f3a22b7d2ebc0ce57b84c761e3f
Bahri Alabey <alabeybb@gmail.com>
2022-07-04 17:56:39 +0300

    jpeg to base64 converter added

--- src/app/add-note/add-note.component.ts
+++ src/app/add-note/add-note.component.ts
@@ -10,6 +10,8 @@ export class AddNoteComponent implements OnInit {
   public Categories: DTOCategory[] = [];
   public NoteValue: string = "";
   public Category: number = 0;
+  public Base64String: string | undefined;
+  public FileType: string | undefined;
 
   constructor(private httpService: HttpClient) {
   }
@@ -28,7 +30,7 @@ export class AddNoteComponent implements OnInit {
 
 
   SubmitForm() {
-
+    console.log(this.Base64String);
     this.httpService.post('http://localhost:5039/api/Note/addnote', {
       Title: this.Title,
       NoteValue: this.NoteValue,
@@ -52,4 +54,32 @@ export class AddNoteComponent implements OnInit {
   }
 
 
+  encodeImageFileAsURL() {
+    if(document){
+    // @ts-ignore
+      let filesSelected = document.getElementById("inputFileToLoad").files;
+    if (filesSelected.length > 0) {
+      let fileToLoad = filesSelected[0];
+      this.FileType = fileToLoad.type;
+      let fileReader = new FileReader();
+
+      fileReader.onload = function (fileLoadedEvent) {
+        // @ts-ignore
+        let srcData = fileLoadedEvent.target.result; // <--- data: base64
+
+        let newImage = document.createElement('img');
+        if (typeof srcData === "string") {
+          newImage.src = srcData;
+        }
+        // @ts-ignore
+        document.getElementById("imgTest").innerHTML = newImage.outerHTML;
+        // @ts-ignore
+        alert("Converted Base64 version is " + document.getElementById("imgTest").innerHTML);
+        // @ts-ignore
+        console.log("Converted Base64 version is " + document.getElementById("imgTest").innerHTML);
+      }
+      fileReader.readAsDataURL(fileToLoad);
+    }
+    }
+    }
 }
