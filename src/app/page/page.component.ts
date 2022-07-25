efbbf768ae0ff46750ef859029db79b091e325dd
Bahri Alabey <alabeybb@gmail.com>
2022-07-25 15:36:07 +0300

    change homepage

--- src/app/page/page.component.ts
+++ src/app/page/page.component.ts
@@ -19,6 +19,17 @@ export class PageComponent implements OnInit {
     this.httpService.get<DTOCategory[]>('http://localhost:5039/api/Category/all').subscribe((data) => {
       if (data) {
         this.Categories = data;
+        this.Categories.sort((a, b) => {
+          let fa = a.name.toLowerCase(), fb = b.name.toLowerCase();
+
+          if (fa < fb) {
+            return -1;
+          }
+          if (fa > fb) {
+            return 1;
+          }
+          return 0;
+        });
       }
     }, error => {
       console.log(error)
